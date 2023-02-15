1d8b848a14a16d66fd723ab361ef38c507c2cff3
Camille <camille.godi@gmail.com>
2023-02-15 13:40:51 +0100

    correction header, mise en place donnes ncbi

--- main.py
+++ main.py
@@ -54,7 +54,6 @@ def table_generator(filePath:str) :
         # partie commune tableau
         with open("HeadResults.html", "r") as header :
             for line in header.readlines():
-                print(line)
                 outputHtml.write(line)
 
         # partie par gène
@@ -62,62 +61,100 @@ def table_generator(filePath:str) :
             geneSymbol = genesList[geneAndOrga][0]; organism = genesList[geneAndOrga][1]
 
             ### DONNEES GENERALES
-            outputHtml.write(f"<tr>\n")
-            outputHtml.write(f"<td>{geneSymbol}</td>\n")
-            outputHtml.write(f"<td><i>{organism}</i></td>\n")
+            outputHtml.write(f"<tr>")
+            outputHtml.write(f"<td>{geneSymbol}</td>")
+            outputHtml.write(f"<td><i>{organism}</i></td>")
+
 
             ### DONNEES NCBI
+            # Official full name
+            outputHtml.write(f"<td>{resNcbi[geneAndOrga]['ncbiFullName']}</td>")
+
+            # NCBI Gene ID
+            outputHtml.write(f"""<td><div class="scroll">""")
+            for ncbiid in resNcbi[geneAndOrga]['ncbiGeneId'] :
+                outputHtml.write(f"{ncbiid}<br>")
+            outputHtml.write(f"</div></td>")
+            
+            # RefSeq Transcript ID
+            outputHtml.write(f"""<td><div class="scroll">""")
+            for trans in resNcbi[geneAndOrga]['ncbiTranscriptAccess'] :
+                outputHtml.write(f"{trans}<br>")
+            outputHtml.write(f"</div></td>")
+
+            # RefSeq prot ID
+            outputHtml.write(f"""<td><div class="scroll">""")
+            for prot in resNcbi[geneAndOrga]['ncbiProteinAccess'] :
+                outputHtml.write(f"{prot}<br>")
+            outputHtml.write(f"</div></td>")
 
 
             ### DONNEES ENSEMBL
             # Ensembl gene ID
-            outputHtml.write(f"<td>{resEnsembl[geneAndOrga]['ensGeneId']}</td>\n")
+            outputHtml.write(f"<td>{resEnsembl[geneAndOrga]['ensGeneId']}</td>")
         
             # Genome Browser
             if resEnsembl[geneAndOrga]['ensUrlBrowser'] == "Data not found" :
-                outputHtml.write(f"<td>Data not found</td>\n")
+                outputHtml.write(f"<td>Data not found</td>")
             else:
-                outputHtml.write(f"<td><a href={resEnsembl[geneAndOrga]['ensUrlBrowser']}>GenomeBrowser</a></td>\n")
+                outputHtml.write(f"<td><a href={resEnsembl[geneAndOrga]['ensUrlBrowser']}>GenomeBrowser</a></td>")
             
             # Liste transcrits
             outputHtml.write(f"""<td><div class="scroll">""")
             for transcrit in resEnsembl[geneAndOrga]['ensTranscriptId'] :
                 outputHtml.write(f"{transcrit}<br>")
-            outputHtml.write(f"</div></td>\n")
+            outputHtml.write(f"</div></td>")
 
             # Liste orthos
             outputHtml.write(f"""<td><div class="scroll">""")
             for ortho in resEnsembl[geneAndOrga]['ensOrthoList']:
                 outputHtml.write(f"{ortho}<br>")
-            outputHtml.write(f"</div></td>\n")
-            outputHtml.write(f"</tr>\n")
+            outputHtml.write(f"</div></td>")
+
 
             ### DONNEES UniProt
             # Prot name
+            outputHtml.write(f"<td>PLACEHOLDER</td>")
+
             # Prot ID
+            outputHtml.write(f"<td>PLACEHOLDER</td>")
 
             ### DONNEES PDB
             # PDB ID
+            outputHtml.write(f"<td>PLACEHOLDER</td>")
 
             ### DONNEES PFAM/INTERPRO
             # Interpro ID
+            outputHtml.write(f"<td>PLACEHOLDER</td>")
 
             ### DONNEES PROSITE
             # Prosite ID
+            outputHtml.write(f"<td>PLACEHOLDER</td>")
 
             ### DONNEES STRING
             # Interactions (lien)
+            outputHtml.write(f"<td>PLACEHOLDER</td>")
 
             ### DONNEES KEGG
             # KEGG IDs
-            # KEGG Pathways
+            outputHtml.write(f"<td>PLACEHOLDER</td>")
 
+            # KEGG Pathways
+            outputHtml.write(f"<td>PLACEHOLDER</td>")
 
             ### DONNEES GO
             # Cellular Component
-            # Molecular Function
-            # Biological Process
+            outputHtml.write(f"<td>PLACEHOLDER</td>")
 
+            # Molecular Function
+            outputHtml.write(f"<td>PLACEHOLDER</td>")
+
+            # Biological Process
+            outputHtml.write(f"<td>PLACEHOLDER</td>")
+
+            # fin de ligne/gène
+            outputHtml.write(f"</tr>")
+            
         # fin tableau
         outputHtml.write("""
 </tbody>
