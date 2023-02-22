10104a34056e1b1ea08e59e087b51d66e7eec70d
Camille <camille.godi@gmail.com>
2023-02-22 13:07:12 +0100

    fixed GeneOntology, enlevé fichier GO en trop

--- GeneOntology.py
+++ GeneOntology.py
@@ -25,7 +25,6 @@ def info_gene_ontology(resUniprot):
             IdsUniprot.append(key)
 
         gene_info = data.querymany(IdsUniprot, scopes="uniprot", fields="go")
-        go = obo_parser.GODag("go-basic.obo")
         
         for result in gene_info:
             if "go" in result:
@@ -34,32 +33,38 @@ def info_gene_ontology(resUniprot):
                 go_mfs = result["go"]["MF"]
 
                 #récupération des informations BP
+                listBP = []
                 for go_bp in go_bps:
                     go_BPid = go_bp["id"]
-                    go_BPcategory = go_bp["gocategory"]
+                    #go_BPcategory = go_bp["gocategory"]
                     BP_term = go_bp["term"]
-                    bioProcess[keys] = {"bioProcess": (go_BPid,BP_term)}
+                    listBP.append([go_BPid, BP_term])
+                bioProcess[keys] = {"bioProcess": listBP}
         #            print(bioProcess)
 
                 #récupération des informations CC
+                listCC = []
                 for go_cc in go_ccs:
                     go_CCid = go_cc["id"]
-                    go_CCcategory = go_cc["gocategory"]
+                    #go_CCcategory = go_cc["gocategory"]
                     CC_term = go_cc["term"]
-                    cellComponent[keys] = {"cellComponent": (go_CCid,CC_term)}     
+                    listCC.append([go_CCid, CC_term])
+                cellComponent[keys] = {"cellComponent": listCC}     
          #            print(cellComponent)
 
                 #récupération des informations MF
+                listMF = []
                 for go_mf in go_mfs:
                     go_MFid = go_mf["id"]
-                    go_MFcategory = go_mf["category"]
+                    #go_MFcategory = go_mf["category"]
                     MF_term = go_mf["term"]
-                    molFunction[keys]= {"molecularFunction": (go_MFid,MF_term)}
-        
+                    listMF.append([go_MFid, MF_term])
+                molFunction[keys]= {"molecularFunction": listMF}
+
 #                   print(molFunction)
     return bioProcess, cellComponent, molFunction
 
 
-# resUniprot = uniprot("GeneSymbols.txt")
-# a = info_gene_ontology(resUniprot)
-# print(a)
+resUniprot = uniprot("GeneSymbols.txt")
+bioProcess, cellComponent, molFunction = info_gene_ontology(resUniprot)
+print(bioProcess, cellComponent, molFunction)
