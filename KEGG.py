a2e32894ba3f11a178d6405504d5220240fd1ef0
Komlan Dieu-Donné Toto <tkdieudonne07@gmail.com>
2023-02-23 04:23:08 +0100

    script KEGG : deuxième modification

--- KEGG.py
+++ KEGG.py
@@ -3,62 +3,55 @@
 import requests
 import re
 from NCBI import ncbi
+
 def kegg_data(resNcbi):
     """
     MODULE POUR L'IMPORT DE DONNEES DEPUIS la base de Données KEGG (utilise des IDs NCBI)
     Données pour chaque ID NCBI:
-    - infoIDs -> dictionnaire retourné assoiant chaque couple (gène/ogranisme ) à son ID KEGG
-    - infoPath -> dictionnaire retourné associant chaque couple (gène/ogranisme ) à un ID pathway et le nom correspondant
+    - infoPath -> dictionnaire retourné associant chaque clé (gène/organisme ) à son ou ses IDs kEGG et les IDs pathways et leurs noms correspondants
     
     """
- 
-#    infoIDkegg = {}  #dictionnaire des IDs KEGG pathways
-      #liste recupérant les correspondances des IDs NCBI dans KEGG
-    keggIDs = []
-    listIdGene = []
+    infoPath = {}  #dictionnaire des IDs KEGG et noms et IDs des pathways
+    
     for keys in resNcbi.keys():
-        listIdGene.append(resNcbi[keys]["ncbiGeneId"][0])  
-    for id in listIdGene:
-        convUrl = f"http://rest.kegg.jp/conv/genes/ncbi-geneid:{id}"
-        convResponse = requests.get(convUrl)
-        try:
-            convData = convResponse.text.split("\t") 
-            keggIDs.append(convData[1][:-1])
-        except:
-            continue
-    infoPath = {}  #dictionnaire des IDs et noms des pathways
-    for keys in resNcbi.keys():  # WARNINGS! je reparcours les keys au lieu de tout mettre sous la toute premiere boucle car lorsque je le fais les ID NCBI ne sont plus récupérés et le code ne marche pas!
-        for keggID in keggIDs: 
-            
-        #    linkInfoKegg = f"https://www.genome.jp/dbget-bin/www_bget?{keggID}"  #Exemple de lien vers KEGG à partir des IDs KEGG
-            
-            pathIDurl = f"http://rest.kegg.jp/link/pathway/{keggID}" #1 ère requête recupérant les informations sur IDs des pathways associés aux IDs kKEGG
-            pathIDresponse = requests.get(pathIDurl)
-            for line in pathIDresponse.text.split("\n"):
-                pathIDs = [] #stocke les IDs des pathways
+        listIdGene = resNcbi[keys]["ncbiGeneId"]
+        keggIDs = []
+        for id in listIdGene:
+            convUrl = f"http://rest.kegg.jp/conv/genes/ncbi-geneid:{id}" #url pour convertir les IDs NCBI en IDs KEGG
+            convResponse = requests.get(convUrl)
+            if convResponse.ok:
                 try:
-                    patternPathID = re.search("path:(.*)", line)
-                    if patternPathID:
-                        pathIDs.append(patternPathID.group(1))
+                    convData = convResponse.text.split("\t")
+                    keggID = convData[1][:-1]  #on recupère tout sauf le dernier élément qui est  "\n"
                 except:
                     continue
-                listPath = []
-                for pathID in pathIDs:
-    #                    infoPath[pathID] = infoPath[keys]
-    #                    pathLink = f"https://www.genome.jp/dbget-bin/www_bget?{pathID}" #lien vers la page web KEGG contenant les onformations sur les pathways
-                    #print(pathLink)
-                    pathUrl = f"http://rest.kegg.jp/get/{pathID}"     
-                    pathNameResponse = requests.get(pathUrl) #2e requête récupérant les informations détaillées sur les pathways 
+                keggIDs.append(keggID)
+        infoPath[keys] = {'infoPathways': []} #on utilise comme clé GeneAndOrga avec une autre clé pathway initialisée à vide à laquelle on ajoutera des valeurs plus tard
+        for keggID in keggIDs: #on parcours la liste des IDs KEGG
+            pathIDurl = f"http://rest.kegg.jp/link/pathway/{keggID}"
+            pathIDresponse = requests.get(pathIDurl)
+            if pathIDresponse.ok:
+                pathIDs = [] #liste qui va récupérer les IDs des pathways coresspondant à l'ID KEGG
+                for line in pathIDresponse.text.split("\n"):
                     try:
-                        patternPathName = re.search("NAME\s+(.*?)\s-", pathNameResponse.text)  #on récupère le nom de la voie
-                        if patternPathName:
-                            path = patternPathName.group(1)
-                            listPath.append((pathID,path))
+                        patternPathID = re.search("path:(.*)", line)
+                        if patternPathID:
+                            pathIDs.append(patternPathID.group(1))
                     except:
                         continue
-                    infoPath =  {keys: {'keggID': keggID, 'pathway': listPath}}
-
+                for pathID in pathIDs:
+                    pathUrl = f"http://rest.kegg.jp/get/{pathID}" #requête pour récupérer les noms des pathways
+                    pathNameResponse = requests.get(pathUrl)
+                    if pathNameResponse.ok:
+                        try:
+                            patternPathName = re.search("NAME\s+(.*?)\s-", pathNameResponse.text)  #regex récupérant les noms
+                            if patternPathName:
+                                path = patternPathName.group(1)
+                                infoPath[keys]['infoPathways'].append({'keggID': keggID, 'pathID': pathID, 'pathName': path})
+                        except:
+                            continue
     return infoPath
+
 
 
 # resNcbi = ncbi("GeneSymbols.txt")
