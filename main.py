da85cb8cbf7a9da214b4f889eadfb8513c521f3e
Camille <camille.godi@gmail.com>
2023-02-26 10:49:51 +0100

    modif message début et sortie html brute + claire

--- main.py
+++ main.py
@@ -27,10 +27,11 @@ filePath = fd.askopenfilename(title="Open your file (format : 'geneSymbol,Organi
 # Par défaut, si pas de fichier donné dans l'interface
 if filePath == () :
     file = "GeneSymbols.txt" # fichier par défaut
+    print("Default file used (GeneSymbols.txt)")
 else :
     file = filePath
+    print("file : ", file)
 
-print(file)
 
 ######################################################################
 
@@ -91,74 +92,74 @@ def table_generator(filePath:str) :
             geneSymbol = genesList[geneAndOrga][0]; organism = genesList[geneAndOrga][1]
 
             ### DONNEES GENERALES
-            outputHtml.write(f"<tr>")
-            outputHtml.write(f"<td>{geneSymbol}</td>")
-            outputHtml.write(f"<td><i>{organism}</i></td>")
+            outputHtml.write(f"<tr>\n")
+            outputHtml.write(f"<td>{geneSymbol}</td>\n")
+            outputHtml.write(f"<td><i>{organism}</i></td>\n")
 
 
             ### DONNEES NCBI
             # Official full name
-            outputHtml.write(f"<td>{resNcbi[geneAndOrga]['ncbiFullName']}</td>")
+            outputHtml.write(f"<td>{resNcbi[geneAndOrga]['ncbiFullName']}</td>\n")
 
             # NCBI Gene ID
             outputHtml.write(f"""<td><div class="scroll">""")
             for ncbiid in resNcbi[geneAndOrga]['ncbiGeneId'] :
                 outputHtml.write(f"{ncbiid}<br>")
-            outputHtml.write(f"</div></td>")
+            outputHtml.write(f"</div></td>\n")
             
             # RefSeq Transcript ID
             outputHtml.write(f"""<td><div class="scroll">""")
             for trans in resNcbi[geneAndOrga]['ncbiTranscriptAccess'] :
                 outputHtml.write(f"{trans}<br>")
-            outputHtml.write(f"</div></td>")
+            outputHtml.write(f"</div></td>\n")
 
             # RefSeq prot ID
             outputHtml.write(f"""<td><div class="scroll">""")
             for prot in resNcbi[geneAndOrga]['ncbiProteinAccess'] :
                 outputHtml.write(f"{prot}<br>")
-            outputHtml.write(f"</div></td>")
+            outputHtml.write(f"</div></td>\n")
 
 
             ### DONNEES ENSEMBL
             # Ensembl gene ID
-            outputHtml.write(f"<td>{resEnsembl[geneAndOrga]['ensGeneId']}</td>")
+            outputHtml.write(f"<td>{resEnsembl[geneAndOrga]['ensGeneId']}</td>\n")
         
             # Genome Browser
             if resEnsembl[geneAndOrga]['ensUrlBrowser'] == "Data not found" :
-                outputHtml.write(f"<td>Data not found</td>")
+                outputHtml.write(f"<td>Data not found</td>\n")
             else:
-                outputHtml.write(f"<td><a href={resEnsembl[geneAndOrga]['ensUrlBrowser']}>GenomeBrowser</a></td>")
+                outputHtml.write(f"<td><a href={resEnsembl[geneAndOrga]['ensUrlBrowser']}>GenomeBrowser</a></td>\n")
             
             # Liste transcrits
             outputHtml.write(f"""<td><div class="scroll">""")
             for transcrit in resEnsembl[geneAndOrga]['ensTranscriptId'] :
                 outputHtml.write(f"{transcrit}<br>")
-            outputHtml.write(f"</div></td>")
+            outputHtml.write(f"</div></td>\n")
 
             # Liste orthos
             outputHtml.write(f"""<td><div class="scroll">""")
             for ortho in resEnsembl[geneAndOrga]['ensOrthoList']:
                 outputHtml.write(f"{ortho}<br>")
-            outputHtml.write(f"</div></td>")
+            outputHtml.write(f"</div></td>\n")
 
 
             ### DONNEES UniProt
             # Prot name
-            outputHtml.write(f"<td>{resUniprot[geneAndOrga]['uniprotName']}</td>")
+            outputHtml.write(f"<td>{resUniprot[geneAndOrga]['uniprotName']}</td>\n")
 
             # Prot ID
-            outputHtml.write(f"<td>{resUniprot[geneAndOrga]['uniprotID']}</td>")
+            outputHtml.write(f"<td>{resUniprot[geneAndOrga]['uniprotID']}</td>\n")
 
 
             ### DONNEES PDB
             # PDB IDs/names
             outputHtml.write(f"""<td><div class="scroll">""")
             if len(resPDB[geneAndOrga]['pdbID']) == 0 :
-                outputHtml.write("Data Not Found")
+                outputHtml.write(f"Data Not Found\n")
             else :
                 for i in range(len(resPDB[geneAndOrga]['pdbID'])):
-                    outputHtml.write(f"<a href=https://www.rcsb.org/structure/{resPDB[geneAndOrga]['pdbID'][i][0]}>{resPDB[geneAndOrga]['pdbID'][i][0]}</a> : {resPDB[geneAndOrga]['pdbID'][i][1]}<br>")
-            outputHtml.write(f"</div></td>")
+                    outputHtml.write(f"<a href=https://www.rcsb.org/structure/{resPDB[geneAndOrga]['pdbID'][i][0]}>{resPDB[geneAndOrga]['pdbID'][i][0]}</a> : {resPDB[geneAndOrga]['pdbID'][i][1]}<br>\n")
+            outputHtml.write(f"</div></td>\n")
 
 
             ### DONNEES PFAM/INTERPRO
@@ -167,7 +168,7 @@ def table_generator(filePath:str) :
             outputHtml.write(f"<a href={resPfam[geneAndOrga]['pfamLink'][0]}>[Protein Link]</a><br>")
             for i in range(len(resPfam[geneAndOrga]['pfamID'])):
                 outputHtml.write(f"<a href=https://www.ebi.ac.uk/interpro/entry/InterPro/{resPfam[geneAndOrga]['pfamID'][i]}/>{resPfam[geneAndOrga]['pfamID'][i]}</a><br>")
-            outputHtml.write(f"</div></td>")
+            outputHtml.write(f"</div></td>\n")
             
 
             ### DONNEES PROSITE
@@ -176,15 +177,15 @@ def table_generator(filePath:str) :
             outputHtml.write(f"<a href=https://prosite.expasy.org/cgi-bin/prosite/PSView.cgi?spac={resProSite[geneAndOrga]['prositeLink']}>[Protein Link]</a><br>")
             for i in range(len(resProSite[geneAndOrga]['prositeID'])):
                 outputHtml.write(f"<a href=https://prosite.expasy.org/{resProSite[geneAndOrga]['prositeID'][i]}>{resProSite[geneAndOrga]['prositeID'][i]}</a><br>")
-            outputHtml.write(f"</div></td>")
+            outputHtml.write(f"</div></td>\n")
 
 
             ### DONNEES STRING
             # Interactions (lien)
             if geneAndOrga in resString.keys() :
-                outputHtml.write(f"<td>{resString[geneAndOrga]['StringID']}<br><a href=https://string-db.org/network/{resString[geneAndOrga]['StringID']}>Interactions (dynamic)</a><br><a href=https://string-db.org/api/highres_image/network?identifiers={resString[geneAndOrga]['StringID']}>Interactions (zoomed, still)</a></td>")
+                outputHtml.write(f"<td>{resString[geneAndOrga]['StringID']}<br><a href=https://string-db.org/network/{resString[geneAndOrga]['StringID']}>Interactions (dynamic)</a><br><a href=https://string-db.org/api/highres_image/network?identifiers={resString[geneAndOrga]['StringID']}>Interactions (zoomed, still)</a></td>\n")
             else : 
-                outputHtml.write(f"<td>Data Not Found<br></td>")
+                outputHtml.write(f"<td>Data Not Found<br></td>\n")
 
             
             ### DONNEES KEGG
@@ -194,7 +195,7 @@ def table_generator(filePath:str) :
                 outputHtml.write(f"Data Not Found")
             else:
                 outputHtml.write(f"<a href=https://www.genome.jp/dbget-bin/www_bget?{resKegg[geneAndOrga]['keggID']}>{resKegg[geneAndOrga]['keggID']}</a><br>")
-            outputHtml.write(f"</div></td>")
+            outputHtml.write(f"</div></td>\n")
 
             # KEGG Pathways
             outputHtml.write(f"""<td><div class="scroll">""")
@@ -203,7 +204,7 @@ def table_generator(filePath:str) :
             else :
                 for i in range(len(resKegg[geneAndOrga]['keggInfoPathways'])):
                     outputHtml.write(f"<a href=https://www.genome.jp/dbget-bin/www_bget?{resKegg[geneAndOrga]['keggInfoPathways'][i]['pathID']}>{resKegg[geneAndOrga]['keggInfoPathways'][i]['pathID']}</a> : {resKegg[geneAndOrga]['keggInfoPathways'][i]['pathName']}<br>")
-            outputHtml.write(f"</div></td>")
+            outputHtml.write(f"</div></td>\n")
 
             ### DONNEES GO
             # Cellular Component
@@ -213,7 +214,7 @@ def table_generator(filePath:str) :
                     outputHtml.write(f"<a href=http://amigo.geneontology.org/amigo/term/{cc[0]}>{cc[0]}</a> : {cc[1]}<br>")
             else:
                 outputHtml.write(f"Data Not Found<br>")
-            outputHtml.write(f"</div></td>")
+            outputHtml.write(f"</div></td>\n")
 
             # Molecular Function
             outputHtml.write(f"""<td><div class="scroll">""")
@@ -222,7 +223,7 @@ def table_generator(filePath:str) :
                     outputHtml.write(f"<a href=http://amigo.geneontology.org/amigo/term/{mf[0]}>{mf[0]}</a> : {mf[1]}<br>")
             else:
                 outputHtml.write(f"Data Not Found<br>")
-            outputHtml.write(f"</div></td>")
+            outputHtml.write(f"</div></td>\n")
 
             # Biological Process
             outputHtml.write(f"""<td><div class="scroll">""")
@@ -231,11 +232,11 @@ def table_generator(filePath:str) :
                     outputHtml.write(f"<a href=http://amigo.geneontology.org/amigo/term/{bp[0]}>{bp[0]}</a> : {bp[1]}<br>")
             else:
                 outputHtml.write(f"Data Not Found<br>")
-            outputHtml.write(f"</div></td>")
+            outputHtml.write(f"</div></td>\n")
 
 
             # fin de ligne/gène
-            outputHtml.write(f"</tr>")
+            outputHtml.write(f"</tr>\n\n")
 
         # fin tableau
         outputHtml.write("""
